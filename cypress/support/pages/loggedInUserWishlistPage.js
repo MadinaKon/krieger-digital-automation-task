63f8feecbdcd24ab0367f7fe4857fe7a23137e73
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-12-25 00:29:30 +0100

    remove unused selectors

--- cypress/support/pages/loggedInUserWishlistPage.js
+++ cypress/support/pages/loggedInUserWishlistPage.js
@@ -12,24 +12,6 @@ class LoggedInUserWishlistPage extends Page {
     "continue shopping": () => {
       return cy.get('[data-testid="goToHome"]');
     },
-
-    // "wishlist icon": () => {
-    //   return cy.get(
-    //     ".headerBrand__element--wishlist > .headerElement > .headerElement__link > .headerElement__icon"
-    //   );
-    // },
-    // "wishlist icon clicked": () => {
-    //   return cy.get(".wishlistIcon--pointer--clicked");
-    // },
-    // "visitor login button": () => {
-    //   // return cy.get('[data-testid="addToWishlistLoginBox"]');
-    //   return cy.contains("Anmelden");
-    // },
-    // "wishlist icon count": () => {
-    //   return cy.get(
-    //     ".headerBrand__element--wishlist > .headerElement > .headerElement__link > .headerElement__text > .headerElement__status"
-    //   );
-    // },
   };
 }
 
